24022aabc7c46a2a613b21b6bf9c5bca08a4a5d4
tct123 <42028373+tct123@users.noreply.github.com>
2025-05-01 11:15:25 +0200

    .

---
+++ src/mynotification/__init__.py
@@ -0,0 +1,13 @@
+class Notification:
+    def init(self, title, message, platform):
+        self.title = title
+        self.message = message
+        self.platform = platform
+
+    def send_mynotification(self):
+        if self.platform == "android":
+            from src.mynotification.notification_android import send_notification
+
+            send_notification(title=self.title, message=self.message)
+        if self.platform:
+            pass
