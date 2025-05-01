ec8e9cad791cf340c49596c283dcdc68397115e8
tct123 <42028373+tct123@users.noreply.github.com>
2025-05-01 11:25:50 +0200

    .

--- src/mynotification/__init__.py
+++ src/mynotification/__init__.py
@@ -5,9 +5,12 @@ class Notification:
         self.platform = platform
 
     def send_mynotification(self):
+        desktop = ["linux", "windows", "macos"]
         if self.platform == "android":
             from src.mynotification.notification_android import send_notification
 
             send_notification(title=self.title, message=self.message)
-        if self.platform:
-            pass
+        if self.platform in desktop:
+            from src.mynotification.notification_android import send_notification
+
+            send_notification(title=self.title, message=self.message)
--- src/mynotification/notification_android.py
+++ src/mynotification/notification_android.py
@@ -6,13 +6,11 @@ def send_notification(title, message):
     Context = autoclass("android.content.Context")
     NotificationBuilder = autoclass("android.app.Notification$Builder")
     NotificationManager = autoclass("android.app.NotificationManager")
-
     activity = PythonActivity.mActivity
     builder = NotificationBuilder(activity)
     builder.setContentTitle(title)
     builder.setContentText(message)
     builder.setSmallIcon(activity.getApplicationInfo().icon)
     notification = builder.build()
-
     service = activity.getSystemService(Context.NOTIFICATION_SERVICE)
     service.notify(0, notification)
