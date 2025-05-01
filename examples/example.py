448822be7ca24004a960e46a32662ee991849cef
tct123 <42028373+tct123@users.noreply.github.com>
2025-05-01 19:53:11 +0200

    Update example.py

--- examples/example.py
+++ examples/example.py
@@ -3,5 +3,5 @@ import sys
 
 platform = sys.platform
 print(platform)
-x = Notification(title="test", message="test",platform=platform)
+x = Notification(title="test", message="test", platform=platform)
 x.send_mynotification()
